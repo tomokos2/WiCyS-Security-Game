10a46ce5d530027db3720573023a74d38490fc2e
talaaoun <61440850+talaaoun@users.noreply.github.com>
2020-03-08 15:22:24 -0500

    Update PlayerController.cs

--- WiCys EOH Project/Assets/Scripts/PlayerController.cs
+++ WiCys EOH Project/Assets/Scripts/PlayerController.cs
@@ -1,4 +1,4 @@
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -32,5 +32,11 @@ public class PlayerController : MonoBehaviour
         {
             transform.Translate(new Vector3(0.0f, Input.GetAxisRaw("Vertical") * moveSpeed * Time.deltaTime, 0.0f));
         }
+ Vector3 minScreenBounds = Camera.main.ScreenToWorldPoint(new Vector3(0, 0, 0));
+     	
+ Vector3 maxScreenBounds = Camera.main.ScreenToWorldPoint(new Vector3(Screen.width, Screen.height, 0));
+ 
+ transform.position = new Vector3(Mathf.Clamp(transform.position.x, minScreenBounds.x + 1,  maxScreenBounds.x - 1),Mathf.Clamp(transform.position.y, minScreenBounds.y + 1, maxScreenBounds.y - 1), transform.position.z);
+ 
     }
 }
