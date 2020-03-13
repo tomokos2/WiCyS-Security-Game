cfc522c5196616204cce21802911fccabf81a9cc
tomokos2 <tomokos2@illinois.edu>
2020-03-12 20:44:46 -0500

    Added Player Animations
    Still slightly buggy, but player now changes direction.

--- WiCys EOH Project/Assets/Scripts/PlayerController.cs
+++ WiCys EOH Project/Assets/Scripts/PlayerController.cs
@@ -5,33 +5,29 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     public float moveSpeed;
+
+    private Animator anim;
     // Start is called before the first frame update
     void Start() {
-    
+        anim = GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetAxisRaw("Horizontal") > 0.5f)
+        if (Input.GetAxisRaw("Horizontal") > 0.5f || Input.GetAxisRaw("Horizontal") < 0.5f)
         {
             transform.Translate (new Vector3(Input.GetAxisRaw("Horizontal") * moveSpeed * Time.deltaTime, 0.0f, 0.0f)); 
         }
 
-        if (Input.GetAxisRaw("Horizontal") < 0.5f)
-        {
-            transform.Translate(new Vector3(Input.GetAxisRaw("Horizontal") * moveSpeed * Time.deltaTime, 0.0f, 0.0f));
-        }
-
-        if (Input.GetAxisRaw("Vertical") > 0.5f)
+        if (Input.GetAxisRaw("Vertical") > 0.5f || Input.GetAxisRaw("Vertical") < 0.5f)
         {
             transform.Translate(new Vector3(0.0f, Input.GetAxisRaw("Vertical") * moveSpeed * Time.deltaTime, 0.0f));
         }
 
-        if (Input.GetAxisRaw("Vertical") < 0.5f)
-        {
-            transform.Translate(new Vector3(0.0f, Input.GetAxisRaw("Vertical") * moveSpeed * Time.deltaTime, 0.0f));
-        }
+        anim.SetFloat("Move_X", Input.GetAxisRaw("Horizontal"));
+        anim.SetFloat("Move_Y", Input.GetAxisRaw("Vertical")); 
+
  Vector3 minScreenBounds = Camera.main.ScreenToWorldPoint(new Vector3(0, 0, 0));
      	
  Vector3 maxScreenBounds = Camera.main.ScreenToWorldPoint(new Vector3(Screen.width, Screen.height, 0));
