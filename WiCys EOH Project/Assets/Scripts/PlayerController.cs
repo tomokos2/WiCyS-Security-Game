335f7bd5a4da0b68ad8a0389e5bf37a9fae0add5
tomokos2 <tomokos2@illinois.edu>
2020-03-16 19:39:38 -0500

    Revert "Revert "Fixed animation errors""
    This reverts commit d17e700afb050a38b789dd75532da98d961fbcfa.

--- WiCys EOH Project/Assets/Scripts/PlayerController.cs
+++ WiCys EOH Project/Assets/Scripts/PlayerController.cs
@@ -28,11 +28,11 @@ public class PlayerController : MonoBehaviour
         anim.SetFloat("Move_X", Input.GetAxisRaw("Horizontal"));
         anim.SetFloat("Move_Y", Input.GetAxisRaw("Vertical")); 
 
- Vector3 minScreenBounds = Camera.main.ScreenToWorldPoint(new Vector3(0, 0, 0));
+        Vector3 minScreenBounds = Camera.main.ScreenToWorldPoint(new Vector3(0, 0, 0));
      	
- Vector3 maxScreenBounds = Camera.main.ScreenToWorldPoint(new Vector3(Screen.width, Screen.height, 0));
+        Vector3 maxScreenBounds = Camera.main.ScreenToWorldPoint(new Vector3(Screen.width, Screen.height, 0));
  
- transform.position = new Vector3(Mathf.Clamp(transform.position.x, minScreenBounds.x + 1,  maxScreenBounds.x - 1),Mathf.Clamp(transform.position.y, minScreenBounds.y + 1, maxScreenBounds.y - 1), transform.position.z);
+        transform.position = new Vector3(Mathf.Clamp(transform.position.x, minScreenBounds.x + 1,  maxScreenBounds.x - 1),Mathf.Clamp(transform.position.y, minScreenBounds.y + 1, maxScreenBounds.y - 1), transform.position.z);
  
     }
 }
