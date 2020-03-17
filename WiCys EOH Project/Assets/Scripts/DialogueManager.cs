08608cc9118fda8fab5b27025bdc178c7d4e0fed
tomokos2 <tomokos2@illinois.edu>
2020-03-16 22:01:34 -0500

    Added glitchy dialogue box with glitchy signs
    In progress. Dialogue box only appears once per sign? Player can still move if there is a dialogue box.

---
+++ WiCys EOH Project/Assets/Scripts/DialogueManager.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DialogueManager : MonoBehaviour
+{
+    public GameObject dBox;
+    public Text dText;
+
+    public bool dialogActive;
+    // Start is called before the first frame update
+    void Start()
+    {
+        dBox.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (dialogActive && Input.GetKeyDown(KeyCode.Space))
+        {
+            dBox.SetActive(false);
+            dialogActive = false;
+        } 
+    }
+
+    public void ShowBox(string dialogue)
+    {
+        dialogActive = true;
+        dText.text = dialogue;
+        dBox.SetActive(true);
+        
+    }
+}
