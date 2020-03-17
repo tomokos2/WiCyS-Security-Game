b66b20a4e54680c47b45e1ffc10a627dbd92cd0b
tomokos2 <tomokos2@illinois.edu>
2020-03-16 20:08:34 -0500

    Changed movement to rigidbody
    No longer vibrates against objects

--- WiCys EOH Project/Assets/Scripts/PlayerController.cs
+++ WiCys EOH Project/Assets/Scripts/PlayerController.cs
@@ -7,9 +7,11 @@ public class PlayerController : MonoBehaviour
     public float moveSpeed;
 
     private Animator anim;
+    private Rigidbody2D rig;
     // Start is called before the first frame update
     void Start() {
         anim = GetComponent<Animator>();
+        rig = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
@@ -17,12 +19,24 @@ public class PlayerController : MonoBehaviour
     {
         if (Input.GetAxisRaw("Horizontal") > 0.5f || Input.GetAxisRaw("Horizontal") < 0.5f)
         {
-            transform.Translate (new Vector3(Input.GetAxisRaw("Horizontal") * moveSpeed * Time.deltaTime, 0.0f, 0.0f)); 
+            //transform.Translate (new Vector3(Input.GetAxisRaw("Horizontal") * moveSpeed * Time.deltaTime, 0.0f, 0.0f)); 
+            rig.velocity = new Vector2(Input.GetAxisRaw("Horizontal") * moveSpeed, rig.velocity.y);
         }
 
         if (Input.GetAxisRaw("Vertical") > 0.5f || Input.GetAxisRaw("Vertical") < 0.5f)
         {
-            transform.Translate(new Vector3(0.0f, Input.GetAxisRaw("Vertical") * moveSpeed * Time.deltaTime, 0.0f));
+            //transform.Translate(new Vector3(0.0f, Input.GetAxisRaw("Vertical") * moveSpeed * Time.deltaTime, 0.0f));
+            rig.velocity = new Vector2(rig.velocity.x, Input.GetAxisRaw("Vertical") * moveSpeed);
+        }
+
+        if (Input.GetAxisRaw("Horizontal") < 0.5f && Input.GetAxisRaw("Horizontal") > -0.5f)
+        {
+            rig.velocity = new Vector2(0f, rig.velocity.y);
+        }
+
+        if (Input.GetAxisRaw("Vertical") < 0.5f && Input.GetAxisRaw("Vertical") > -0.5f)
+        {
+            rig.velocity = new Vector2(rig.velocity.x, 0f);
         }
 
         anim.SetFloat("Move_X", Input.GetAxisRaw("Horizontal"));
